38fcc3f80ea7c8aac2b8827b89f82661416b20d5
grupo7devops <manuelbarrionuevo678@hotmail.com>
2024-02-16 12:41:17 -0300

    update

--- vars/pinVars.groovy
+++ vars/pinVars.groovy
@@ -1,12 +1,7 @@
 def call() {
     def pinVars = [:]
 
-    pinVars.buildDockerImage = { imageName, version ->
-        sh '''
-                ls -la
-                docker build -t $imageName:$version .
-            '''
-    }
+   
 
     pinVars.pushDockerImage = { imageName, version, directory ->
         sh """
