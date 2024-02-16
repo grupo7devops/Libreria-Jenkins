a296a4bd324ae8c1e06a56544a1e0669e5592f19
grupo7devops <manuelbarrionuevo678@hotmail.com>
2024-02-16 12:37:34 -0300

    update

--- vars/pinVars.groovy
+++ vars/pinVars.groovy
@@ -1,23 +1,27 @@
-// DockerHelper.groovy
+def call() {
+    def pinVars = [:]
 
-def buildAndPushDockerImage(String imageName, String version, String directory) {
-    try {
-        // Tu lógica de construcción de la imagen Docker aquí
-        sh "docker build -t $imageName:$version $directory"
-        
-        // Tu lógica de empujar la imagen Docker aquí
-        sh "docker push $imageName:$version"
-    } catch (Exception e) {
-        throw new RuntimeException("Error al construir o empujar la imagen Docker: ${e.message}")
+    pinVars.buildDockerImage = { imageName, version ->
+        sh '''
+                ls -la
+                docker build -t $imageName:$version .
+            '''
     }
-}
 
-def dockerLogin(String registryUrl) {
-    try {
-        // Tu lógica de inicio de sesión con Docker aquí
-        // Puedes utilizar bibliotecas como docker-py para esto
-        return true
-    } catch (Exception e) {
-        throw new RuntimeException("Error al iniciar sesión en Docker: ${e.message}")
+    pinVars.pushDockerImage = { imageName, version, directory ->
+        sh """
+            docker push $imageName:$version
+        """
     }
+
+    pinVars.dockerLogin = { registryUrl ->
+        withCredentials([usernamePassword(credentialsId: 'dockerHub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')]) {
+            withDockerRegistry([url: registryUrl]) {
+                return true
+            }
+        }
+        return false
+    }
+
+    return pinVars
 }
